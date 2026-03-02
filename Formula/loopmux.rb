d84b613e96f302ca35a47a1afd88211a16973da3
Diego Marinho <dmztheone@gmail.com>
2026-03-02 14:18:44 +1100

    loopmux v0.1.44

--- Formula/loopmux.rb
+++ Formula/loopmux.rb
@@ -1,8 +1,8 @@
 class Loopmux < Formula
   desc "Loop prompts into tmux panes with triggers and delays"
   homepage "https://github.com/dmoliveira/loopmux"
-  url "https://github.com/dmoliveira/loopmux/archive/refs/tags/v0.1.33.tar.gz"
-  sha256 "74d6ccb10ef6837f9d645f2ea73f85c92799a6cbaaec5d5a72ae7f940a98e409"
+  url "https://github.com/dmoliveira/loopmux/archive/refs/tags/v0.1.44.tar.gz"
+  sha256 "41c270062cb6d7eefabad74005de758e58bcd3c2dae5b5b2a66339adc02ebb68"
   license "MIT"
 
   depends_on "rust" => :build
